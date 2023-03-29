562bfbead537af2d1ee0b52be2672518c1937bce
Faraz Khan <faraz.khan14158124@gmail.com>
2023-03-29 20:26:13 +0530

    Creating server

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
+const http = require("http");
 
+///////////////////////////////////////////////////////////
+///////////////////FILES////////////////////////////////
 /*
 // Synchronous :
 
@@ -12,23 +15,38 @@ const textOUT = `this is what we know about avacado : ${textIN}.\nCreated on ${D
 fs.writeFileSync("./txt/output.txt", textOUT);
 console.log("File Written successfuly");
 
-*/
 
 // Asynchronous :
 
 // fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-//   console.log(data);
-// });
-// console.log("Reading Data File...");
-
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+    //   console.log(data);
+    // });
+    // console.log("Reading Data File...");
+    
+    fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+        fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
+            console.log(data2);
+            fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
       console.log(data3);
     });
     fs.writeFile("./txt/final.txt", "utf-8", (err) => {
-      console.log("File Written Successfully !");
+        console.log("File Written Successfully !");
     });
-  });
+});
+});
+
+console.log("Will Read File");
+
+
+*/
+
+///////////////////////////////////////////////////////////
+///////////////////SERVER////////////////////////////////
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server !");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
 });
