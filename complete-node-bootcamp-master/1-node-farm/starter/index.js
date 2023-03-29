baabc2371cbffc8a064195f19102534106345284
Faraz Khan <faraz.khan14158124@gmail.com>
2023-03-29 18:36:36 +0530

    async file read and write

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 
+/*
+// Synchronous :
+
 // Read Files
 const textIN = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIN);
@@ -9,4 +12,23 @@ const textOUT = `this is what we know about avacado : ${textIN}.\nCreated on ${D
 fs.writeFileSync("./txt/output.txt", textOUT);
 console.log("File Written successfuly");
 
-// Node
+*/
+
+// Asynchronous :
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+// console.log("Reading Data File...");
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+    });
+    fs.writeFile("./txt/final.txt", "utf-8", (err) => {
+      console.log("File Written Successfully !");
+    });
+  });
+});
