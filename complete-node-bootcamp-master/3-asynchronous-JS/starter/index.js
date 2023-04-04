24421eb7d058a57abf8e5a646593e28a768a8c6a
Faraz Khan <faraz.khan14158124@gmail.com>
2023-04-04 21:33:07 +0530

    promises

--- complete-node-bootcamp-master/3-asynchronous-JS/starter/index.js
+++ complete-node-bootcamp-master/3-asynchronous-JS/starter/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
+/*
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed : ${data}`);
 
@@ -13,5 +14,24 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
       fs.writeFile("dog-image.txt", res.body.message, (err, data) => {
         console.log("Random Dog Image Saved to file");
       });
+    });
+});
+ */
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed : ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
+      console.log(res.body.message);
+
+      fs.writeFile("dog-image.txt", res.body.message, (err, data) => {
+        if (err) return console.log(err.message);
+        console.log("Random Dog Image Saved to file");
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
