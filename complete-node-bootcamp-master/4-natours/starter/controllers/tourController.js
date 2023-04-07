3a2231783643b6c3613f7279b8521c1169d4b3d2
Faraz Khan <faraz.khan14158124@gmail.com>
2023-04-07 19:55:27 +0530

    chaining middlewares

--- complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
+++ complete-node-bootcamp-master/4-natours/starter/controllers/tourController.js
@@ -17,6 +17,16 @@ exports.checkId = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name && !req.body.price)
+    return res.status(400).json({
+      status: 'fail',
+      message:
+        'Invalid format, missing name or price properties',
+    });
+  next();
+};
+
 //////////////////////////////////////////////////////////
 ////////  ROUTE HANDLERS  ///////////////////////////////////////////
 //////////////////////////////////////////////////////////
